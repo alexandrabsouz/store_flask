eeae130b10c7ae526830bfbee5ec6a7981adcc3b
alexandra <alexandrabsouz@gmail.com>
2021-08-18 20:42:40 -0300

    config: db and bcrypt

--- store/__init__.py
+++ store/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///store.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+app.config['SECRET_KEY'] = 'secret357951258654ey'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 from store.admin import routes
--- store/admin/routes.py
+++ store/admin/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, session, request, url_for, flash
+from flask import render_template, session, request, url_for, flash, redirect
 from .form import RegistrationForm
-from store import app, db 
+from store import app, db, bcrypt
+from .models import User
+import os
 
 
 @app.get('/')
@@ -12,9 +14,11 @@ def index():
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        #user = User(form.username.data, form.email.data,
-                    #form.password.data)
-        #db_session.add(user)
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data,
+                    password=hash_password)
+        
+        db.session.add(user)
         flash('Thanks for registering')
-        return redirect(url_for('login'))
+        return redirect(url_for('index'))
     return render_template('admin/register.html', form=form, title='Register')
